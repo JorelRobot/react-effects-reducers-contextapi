d5a6c14e44b93c81554a21f163fd57ccab9008d3
Joel Rueda Robles <joel.rueda.robles.sc@gmail.com>
2022-09-22 14:38:44 -0500

    REFACTOR: Using forward refs in order to use a function from a child component

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -1,14 +1,33 @@
-import React from "react";
+import React, { useRef, useImperativeHandle } from "react";
 
 import classes from './Input.module.css';
 
-const Input = (props) => {
+const Input = React.forwardRef((props, ref) => {
+
+    const inputRef = useRef();
+
+    const activate = () => {
+        inputRef.current.focus();
+    };
+
+    useImperativeHandle(ref, () => {
+        /**
+         * In this functions, we return an object which will be accesable
+         * by other components. This object can contain variables, objects
+         * or even functions
+         **/
+        return {
+            focus: activate
+        };
+    });
+
     return (
         <div
             className={`${classes.control} ${props.isValid === false ? classes.invalid : ''
                 }`}>
             <label htmlFor={props.id}>{props.label}</label>
             <input
+                ref={inputRef}
                 type={props.type}
                 id={props.id}
                 value={props.value}
@@ -17,6 +36,6 @@ const Input = (props) => {
             />
         </div>
     );
-};
+});
 
 export default Input;
